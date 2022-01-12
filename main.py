e77a2c78e9bb758086b2ff3417f24edd86a2deee
gysu <qzero735280@gmail.com>
2022-01-12 16:17:53 +0800

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI  #導入fastapi
+import uvicorn
+from routers import webhooks
+from enum import Enum
+#uvicorn main:app --reload
+#uvicorn main:app --debug 
+# 添加--debug，它将在文件更新时自动重新加载，这在开发过程中非常方便。
+app = FastAPI()         #創建一個fastapi應用
+
+app.include_router(webhooks.router) #注册 APIRouter
+
+@app.get("/item/{item_id}")   #創建一個route路徑用get方法
+async def root(item_id:int):      #function 傳入參數 :int 型態
+    return {"message": "Hello World",
+            "item_id": item_id}
+
+
+class ModelName(str,Enum):
+  alexnet = "alexnet"
+  resnet  = "resnet"
+  lenet   = "lenet"
+
+
+@app.get("/model_name/{model_name}")   #創建一個route路徑用get方法
+async def get_model(model_name:ModelName):      #function 傳入參數 :int 型態
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+#路径转换器
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
---
+++ routers/webhooks.py
@@ -0,0 +1,55 @@
+
+from typing import List, Optional
+
+from fastapi import APIRouter, HTTPException, Header, Request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import TextMessage, MessageEvent, TextSendMessage, StickerMessage,StickerSendMessage
+from pydantic import BaseModel
+
+import configparser
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+router = APIRouter(
+    prefix="/webhooks",
+    tags=["chatbot"],
+    responses={404: {"description": "Not found"}},
+)
+
+class Line(BaseModel):
+    destination: str
+    events: List[Optional[None]]
+
+
+@router.post("/line")
+async def callback(request: Request, x_line_signature: str = Header(None)):
+    body = await request.body()
+    try:
+        handler.handle(body.decode("utf-8"), x_line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="chatbot handle body error.")
+    return 'OK'
+
+# 學你說話
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    print("!!!!!!!!!!!!!!!!!!!!!!")
+    print(event)
+    print("!!!!!!!!!!!!!!!!!!!!!!")
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
+@handler.add(MessageEvent, message=StickerMessage)
+def sticker_text(event):
+    # Judge condition
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(package_id='6136', sticker_id='10551379')
+    )
